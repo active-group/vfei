2d9060a0f11e747da5229bea15e28d537dedd569
Mike Sperber <sperber@deinprogramm.de>
2017-05-24 15:41:06 +0200

    0.4.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/vfei "0.4.0-SNAPSHOT"
+(defproject de.active-group/vfei "0.4.0"
   :description "VFEI library"
   :url "https://github.com/active-group/vfei"
   :license {:name "Eclipse Public License"
